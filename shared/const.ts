f3e570964ca950504440131605bf0af1cfe0a7e3
MikhailDubina <148867981+MikhailDubina@users.noreply.github.com>
2025-11-13 15:16:57 +0000

    Add missing shared directory with constants and errors

---
+++ shared/const.ts
@@ -0,0 +1,5 @@
+export const COOKIE_NAME = "session";
+export const ONE_YEAR_MS = 365 * 24 * 60 * 60 * 1000;
+export const AXIOS_TIMEOUT_MS = 30000;
+export const UNAUTHED_ERR_MSG = "You must be logged in to access this resource";
+export const NOT_ADMIN_ERR_MSG = "You must be an admin to access this resource";
